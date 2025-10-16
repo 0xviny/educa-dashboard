0ea9ea680b4a6b6be689cdcf0fe5c515b24d9c65
Vinicius Gabriel Galvão Marcelino <122948478+0xviny@users.noreply.github.com>
2025-10-16 09:46:14 -0300

    Update and rename next.config.mjs to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  distDir: "dist",
   eslint: {
     ignoreDuringBuilds: true,
   },
